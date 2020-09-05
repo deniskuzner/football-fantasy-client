73eee137fceece2d9981771413dce37b45ebacd4
deniskuzner <deniskuzner@gmail.com>
2020-09-05 22:38:33 +0200

    Izmenjen fixtures prikaz. Dodati Match Event modeli.

---
+++ src/app/services/match-event.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { AppConstants } from '../constants/app-constants';
+import { MatchEvent } from '../models/match-event.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchEventService {
+
+  constructor(private http: HttpClient) { }
+
+  parseGameweekMatchEvents(gameweekId: number) {
+    return this.http.get<MatchEvent[]>(AppConstants.serverUrl + '/match-events/parse-match-events/gameweek/' + gameweekId);
+  }
+
+
+}
